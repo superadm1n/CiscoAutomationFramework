1cb4128471369efc1bac10f35e4b707b01d375ce
Kyle Kowalczyk <kyleko@capitalcu.com>
2022-09-14 14:30:48 -0500

    Incremented version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Kyle Kowalczyk'
 author = 'Kyle Kowalczyk'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.0.4'
 
 
 # -- General configuration ---------------------------------------------------
